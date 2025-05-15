84dd5e14077818938c784bef37d9f773283fa5c2
ipopadic-ip <popadic2003@gmail.com>
2025-05-15 23:31:25 +0200

    Cleaned up nested project structure

--- src/main/java/com/unidunav/student/repository/StudentRepository.java
+++
@@ -1,7 +0,0 @@
-package com.unidunav.student.repository;
-
-import com.unidunav.student.model.Student;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface StudentRepository extends JpaRepository<Student, Long> {
-}
